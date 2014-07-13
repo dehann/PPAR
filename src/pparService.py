f1b5aa1cd87ed7c372cc8822e53c4b99ee77e5d8
dehann <dehann@mit.edu>
2014-07-13 15:06:29 -0400

    quick test is working

--- src/pparService.py
+++ src/pparService.py
@@ -1,6 +1,8 @@
+import time
 import json
 import lcm
 from ppar import msg_t
+
 
 json_data=open('../data/example.json')
 jsonFileContent = json.load(json_data)
@@ -11,21 +13,28 @@ def getFromJsonFile(reqstr):
     global jsonFileContent
     return eval("jsonFileContent" + reqstr)
 
+def sendReplyMsg(old, result):
+    old.utime = int(time.time() * 1000000)
+    old.reqOrig = "pparService.py"
+    old.str = result
+    global lc
+    lc.publish("PPAR_REPLY_MSIM", old.encode())
+
 def my_handler(channel, data):
     msg = msg_t.decode(data)
     print("Received message on channel \"%s\"" % channel)
     print("   timestamp   = %s" % str(msg.utime))
     print("   random ID   = %s" % str(msg.reqRndID))
     print("   reqOrig     = %s" % str(msg.reqOrig))
-    print("   reqeust     = %s" % str(msg.str))
+    print("   request     = %s" % str(msg.str))
     print("")
     reqstr = str(msg.str)
-    print getFromJsonFile(reqstr)
+    result = str(getFromJsonFile(reqstr))
+    sendReplyMsg(msg, result)
 
 lc = lcm.LCM()
 subscription = lc.subscribe("PPAR_REQUEST_MSIM", my_handler)
 print "Subscribed to : PPAR_REQUEST_MSIM"
-
 
 try:
     while True:
@@ -34,6 +43,5 @@ except KeyboardInterrupt:
     pass
 
 lc.unsubscribe(subscription)
-
 
 
